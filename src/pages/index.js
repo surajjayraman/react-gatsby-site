c06bb0dc5dc48bcf2ef0ac3db61390335000992b
surajjayraman <suraj.jayraman@gmail.com>
2023-08-20 21:25:28 -0400

    Update starter files

--- src/pages/index.js
+++ src/pages/index.js
@@ -118,7 +118,7 @@ const links = [
     url: "https://www.gatsbyjs.com/cloud",
     badge: true,
     description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby's Cloud. Get started for free!",
+      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
     color: "#663399",
   },
 ]
