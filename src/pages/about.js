e127e141914981de63b2d0f4d971deb335461374
surajjayraman <suraj.jayraman@gmail.com>
2023-08-22 11:05:32 -0400

    Add an About Me page

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import * as React from  'react';
+
+const AboutPage = () => {
+    return (
+      <h1>I'm a junior react dev, trying to build something!!!</h1>
+    )
+  }
+
+export const Head = () => <title>About Me</title>;
+export default AboutPage;
