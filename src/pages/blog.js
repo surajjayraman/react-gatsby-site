e4b2753230be78f0250d2efc359193f358348519
surajjayraman <suraj.jayraman@gmail.com>
2023-08-21 22:13:52 -0400

    Add blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const BlogPage = () => {
+  return (
+    <h1>This is my blog, stay tuned!!!</h1>
+  )
+}
+
+export const Head = () => <title>My Blog</title>;
+
+export default BlogPage;
