14a87f55c4e711e0e2a87cc6f03e1169729a7b88
surajjayraman <suraj.jayraman@gmail.com>
2023-08-24 12:24:01 -0400

    Add static image to landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => {
   return (
     <Layout pageTitle='HomePage'>      
       <p>Hi, welcome to my site!</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/clifford.jpg"
+      />
     </Layout>    
   )
 }
